88765db88bde3b541047ccc17df8bcf572ab003e
Єгор Кирилін <yehor.kyrylin@ukma.edu.ua>
2024-03-17 15:06:25 +0200

    Added empty functions for input and output

---
+++ app/io/input.py
@@ -0,0 +1,9 @@
+def input_from_console():
+    pass
+
+def input_from_file(filepath):
+    pass
+
+def input_from_file_pandas(filepath):
+    pass
+
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def output_to_console(text):
+    pass
+
+def output_to_file(text, filepath):
+    pass
